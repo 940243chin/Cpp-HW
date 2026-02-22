9c99f1d3763c0c455963bedf479563d6f1c53d02
Robert <940243chin@gmail.com>
2026-02-22 17:14:53 +0800

    寫了Problem5是一個計算哪一列有做多人符合門檻，如果數量一樣以最小列為主

--- HW3（Functions）.cpp
+++ HW3（Functions）.cpp
@@ -77,10 +77,12 @@ int main() {
 
 // Problem4
 
-//gradeCnt是陣列長度，threshold是門檻，grades是陣列，用&是call by reference(現在這個grades[]是vector<int> v的分身，所以如果改到grades[]，v也會跟著改，分身就是除了名字不一樣，其他都一樣)
+// gradeCnt是陣列長度，threshold是門檻，grades是陣列，用&是call by
+// reference(現在這個grades[]是vector<int>
+// v的分身，所以如果改到grades[]，v也會跟著改，分身就是除了名字不一樣，其他都一樣)
 /*int highGradeCnt(int gradeCnt, int threshold, vector<int> &grades) {
-  int count = 0;
-
+//count是用來計幾個過門檻(>=threshold)的，大於就＋＋，回傳count
+int count = 0;
   for (int i = 0; i < gradeCnt; i++) {
     if (grades[i] >= threshold) {
       count++;
@@ -101,3 +103,38 @@ int main() {
 }*/
 
 // Problem5
+// threshold是門檻，assigmentCnt是作業數，gradeCnt是學生數，grades是二維陣列
+/*int mostheighGrades(int threshold, int assigmentCnt, int gradeCnt, int
+grades[][50]) {
+  //count是用來計幾個過門檻(>=threshold)的，大於就＋＋，回傳count，max是用來記錄最大的count，maxindex是用來記錄最大的count對應的index
+  int count = 0, max = 0, maxindex = 0;
+  for (int i = 0; i < assigmentCnt; i++) {
+    for (int j = 0; j < gradeCnt; j++) {
+      if (grades[i][j] >= threshold) {
+        count++;
+      }
+    }
+    if (count > max) {
+      max = count;
+      maxindex = i + 1;
+    }
+    count = 0;
+  }
+  return maxindex;
+}
+
+int main() {
+  int m, n, threshold;
+  cin >> m >> n >> threshold;
+  //因為傳二維陣列當argument時，必須指定第二維的長度，所以要先定義一個二維陣列，因為題目說m<-=10,n<=50，所以就設10x50，如果沒有設n的長度，編譯器會不知道要怎麼傳
+  int grades[10][50];
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> grades[i][j];
+    }
+  }
+  cout << mostheighGrades(threshold, m, n, grades) << "\n";
+  return 0;
+}*/
+
+// End
